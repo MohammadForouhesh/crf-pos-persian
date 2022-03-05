8800a8ca82cf6cd97f3551f1944b357dfa2d6bc9
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-05 12:25:24 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='crf_pos',
+    version='1.0.0',
+    packages=['cfr_pos'],
+    url='https://github.com/MohammadForouhesh/crf-pos-persian',
+    license='MIT',
+    author='mforouhesh',
+    author_email='Mohammadh.Forouhesh@gmail.com',
+    description='Persian Part-of-Speech tagger framework'
+)
