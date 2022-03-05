76aeebecb25794c7f9875e3b5d8d38ff6eaa09c5
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-05 16:40:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from crf_pos.crf import POSTagger
+from crf_pos.api import downloader
 
-pos_tagger = POSTagger("model/perpos.model")
+downloader('https://github.com/MohammadForouhesh/crf-pos-persian/releases/download/v1/perpos.model')
+pos_tagger = POSTagger("model/perpos-v1.model")
 tokens = "ابراهیم رئیسی رئيس‌جمهور جمهوری اسلامی ایران می‌باشد".split()
 print(pos_tagger[tokens][0][1])
