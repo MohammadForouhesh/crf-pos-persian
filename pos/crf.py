370beee00c318149de89c11869a18dd7223166de
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-05 11:15:21 +0330

    Update crf.py

---
+++ pos/crf.py
@@ -0,0 +1,19 @@
+import pickle
+
+from pos.utils import token2features
+
+
+class POSTagger:
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.crf = pickle.load(open(model_path, "rb"))
+
+    def __getitem__(self, item):
+        return self.parse_sentences([item])[0]
+
+    def parse_sentences(self, token_stream: list):
+        y_pred = self.crf.predict([token2features(token) for token in token_stream])
+        out = []
+        for x_sent, y_pred in zip(token_stream, y_pred):
+            out.append(list(zip(x_sent, y_pred)))
+        return out
