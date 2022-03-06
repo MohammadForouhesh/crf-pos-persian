df84d3fb1edb547ad36f197361e227078ca3247f
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-06 13:49:56 +0330

    Update crf.py

--- crf_pos/crf.py
+++ crf_pos/crf.py
@@ -1,13 +1,20 @@
+from typing import Union
+import parsivar
 from crf_pos.utils import token2features
 import pickle
+
+norm = parsivar.Normalizer(statistical_space_correction=True, date_normalizing_needed=True)
+tokenizer = parsivar.Tokenizer()
 
 
 class POSTagger:
     def __init__(self, model_path):
         self.model_path = model_path
-        self.crf = pickle.load(open(model_path, "rb"))
+        with open(model_path, 'rb') as resource:
+            self.crf = pickle.load(resource)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: Union[list, str]):
+        if isinstance(item, str):   item = tokenizer.tokenize_words(norm.normalize(item))
         return self.parse_sentences([item])[0]
 
     def parse_sentences(self, token_stream: list):
