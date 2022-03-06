0924fb8391fc6208596468992b688b8f00552db3
Mohammad Forouhesh <Mohammad.Forouhesh@Outlook.com>
2022-03-07 00:25:10 +0330

    move api

--- crf_pos/api.py
+++ crf_pos/api.py
@@ -1,10 +1,10 @@
 import requests
 
 
-def downloader(path: str):
+def downloader(path: str, save_path: str = 'model/perpos-v1.model', mode: str = 'wb'):
     try:
         model_bin = requests.get(path, allow_redirects=True)
-        with open('model/perpos-v1.model', 'wb') as resource:
+        with open(save_path, mode) as resource:
             resource.write(model_bin.content)
     except:
         raise Exception('not a proper webpage')
