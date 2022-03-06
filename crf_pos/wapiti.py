cfd2bed17cd39b2bf85f847d90cda0cb36a2e395
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-06 15:47:52 +0330

    Update wapiti.py

--- crf_pos/wapiti.py
+++ crf_pos/wapiti.py
@@ -1,15 +1,15 @@
-from typing import Union
-
+from crf_pos.utils import is_all_latin
+from typing import Union, List, Generator, Any
 from wapiti import Model
 import parsivar
-from crf_pos.utils import is_all_latin
 
-norm = parsivar.Normalizer(statistical_space_correction=True, date_normalizing_needed=True)
+
+norm = parsivar.Normalizer()
 tokenizer = parsivar.Tokenizer()
 
 
 class WapitiPosTagger():
-    def __init__(self, model_path: str = 'model/') -> None:
+    def __init__(self, model_path: str = 'model/UPC_full_model_wapiti') -> None:
         self.model_path = model_path
         self.wapiti = Model(model=self.model_path)
 
@@ -17,15 +17,12 @@ class WapitiPosTagger():
         if isinstance(item, str):   item = tokenizer.tokenize_words(norm.normalize(item))
         return self.parse([item])[0]
 
-    def parse(self, token_list):
-        tagged_tuples = []
+    def parse(self, token_list: List[str]) -> list[list[tuple[Any, Any]]]:
+        sent_line = "\n".join(token_list[0])
+        postags = self.wapiti.label_sequence(sent_line).decode('utf-8').strip().split('\n')
+        return list(WapitiPosTagger.zip_vector(zip(token_list, [list(map(lambda item: u"FW" if is_all_latin(item) else item[0], postags))])))
 
-        sent_line = "\n".join(x for x in token_list)
-        postags = self.wapiti.label_sequence(sent_line).decode('utf-8')
-        postags = postags.strip().split('\n')
-        for i, el in enumerate(token_list):
-            if is_all_latin(el):
-                tagged_tuples.append((el, u"FW"))
-            else:
-                tagged_tuples.append((el, postags[i]))
-        return tagged_tuples
+    @staticmethod
+    def zip_vector(iterable: zip):
+        for key, item in iterable:
+            yield list(zip(key, item))
