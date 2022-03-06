2de3660b9f268d4c39feb1d975ba41219cbbe5da
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-06 17:18:36 +0330

    Update wapiti.py

--- crf_pos/wapiti.py
+++ crf_pos/wapiti.py
@@ -1,5 +1,4 @@
-from crf_pos.utils import is_all_latin
-from typing import Union, List, Generator, Any
+from typing import Union, List, Any, Tuple, Generator
 from wapiti import Model
 import parsivar
 
@@ -13,16 +12,16 @@ class WapitiPosTagger():
         self.model_path = model_path
         self.wapiti = Model(model=self.model_path)
 
-    def __getitem__(self, item: Union[list, str]):
+    def __getitem__(self, item: Union[list, str]) -> List[Tuple[str, str]]:
         if isinstance(item, str):   item = tokenizer.tokenize_words(norm.normalize(item))
         return self.parse([item])[0]
 
-    def parse(self, token_list: List[str]) -> list[list[tuple[Any, Any]]]:
+    def parse(self, token_list: List[str]) -> List[List[Tuple[Any, Any]]]:
         sent_line = "\n".join(token_list[0])
         postags = self.wapiti.label_sequence(sent_line).decode('utf-8').strip().split('\n')
-        return list(WapitiPosTagger.zip_vector(zip(token_list, [list(map(lambda item: u"FW" if is_all_latin(item) else item[0], postags))])))
+        return list(WapitiPosTagger.zip_vector(zip(token_list, [list(map(lambda item: item, postags))])))
 
     @staticmethod
-    def zip_vector(iterable: zip):
+    def zip_vector(iterable: zip) -> Generator[List[Tuple[str, str]]]:
         for key, item in iterable:
             yield list(zip(key, item))
