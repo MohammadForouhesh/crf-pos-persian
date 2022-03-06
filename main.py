014c88edb4073cac4659079dabb33c7378f5e091
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-06 15:48:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import unittest
 from crf_pos.crf import CrfPosTagger
 from crf_pos.api import downloader
-
-pos_tagger = CrfPosTagger("model/perpos-v1.model")
-tokens = "ابراهیم رئیسی رئيس‌جمهور جمهوری اسلامی ایران می‌باشد"
-print(pos_tagger[tokens])
-print(pos_tagger['دانش‌آموز و دانش آموز متفاوت می باشند'])
-print(pos_tagger['نهاد ریاست جمهوری'])
+from crf_pos.wapiti import WapitiPosTagger
 
 
-class MyTestCase(unittest.TestCase):
+class CrfTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.model_path = 'https://github.com/MohammadForouhesh/crf-pos-persian/releases/download/v1/perpos.model'
         self.tagger = CrfPosTagger("model/perpos-v1.model")
+        self.wapiti = WapitiPosTagger(model_path="model/UPC_full_model_wapiti")
         self.all_tags = ('N', 'P', 'V', 'ADV', 'ADVe', 'RES', 'RESe', 'DET', 'DETe', 'AJ', 'AJe', 'CL', 'INT', 'CONJ',
                          'CONJe', 'POSTP', 'PRO', 'PROe', 'NUM', 'NUMe', 'PUNC', 'Ne', 'Pe')
 
-    def test_api(self):
+    def test_crf_api(self):
         self.assertRaises(Exception, downloader, path='wrong-path')
 
-    def test_normalized_verb_tagging(self):
+    def test_crf_normalized_verb_tagging(self):
         self.assertEqual(self.tagger['می باشد'], self.tagger['می‌باشد'])
         self.assertNotEqual(self.tagger['نهاد ریاست جمهوری'], self.tagger['نهاد ریاست‌جمهوری'])
 
-    def test_tagger(self):
+    def test_crf_tagger(self):
         self.assertIsInstance(self.tagger['رئيس‌جمهور'], list)
         self.assertIsInstance(self.tagger['رئيس‌جمهور جمهوری اسلامی'], list)
         self.assertIsInstance(self.tagger['رئيس‌جمهور جمهوری اسلامی'][0], tuple)
@@ -31,7 +27,29 @@ class MyTestCase(unittest.TestCase):
         for item in self.tagger['رئيس‌جمهور جمهوری اسلامی']:
             self.assertIn(member=item[1], container=self.all_tags)
 
-    def test_ai(self):
+    def test_crf_ai(self):
+        self.assertIn(self.tagger['رئيس‌جمهور'][0][1], 'Ne')
+
+
+class WapitiTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tagger = WapitiPosTagger(model_path="model/UPC_full_model_wapiti")
+        self.all_tags = ('N', 'P', 'V', 'A', 'R', 'FW')
+
+    def test_wapiti_normalizer(self):
+        self.assertEqual(self.tagger['می باشد'], self.tagger['می‌باشد'])
+        self.assertNotEqual(self.tagger['نهاد ریاست جمهوری'], self.tagger['نهاد ریاست‌جمهوری'])
+
+    def test_wapiti_tagger(self):
+        self.assertIsInstance(self.tagger['رئيس جمهور'], list)
+        self.assertIsInstance(self.tagger['رئيس‌جمهور جمهوری اسلامی'], list)
+        self.assertIsInstance(self.tagger['رئيس‌جمهور جمهوری اسلامی'][0], tuple)
+        self.assertIsInstance(self.tagger['رئيس‌جمهور جمهوری اسلامی'][0][1], str)
+        for item in self.tagger['رئيس‌جمهور جمهوری اسلامی']:
+            print(item)
+            self.assertIn(member=item[1], container=self.all_tags)
+
+    def test_wapiti_ai(self):
         self.assertIn(self.tagger['رئيس‌جمهور'][0][1], 'Ne')
 
 
