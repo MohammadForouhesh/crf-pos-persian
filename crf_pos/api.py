cd449b3784c63158da80d53efc0db0a09d0020ae
Mohammad Forouhesh <MForouhesh@aut.ac.ir>
2022-03-07 17:45:22 +0330

    Revert "Revert "api failed in scrutinizer ci""
    This reverts commit 2a2178861ed304b8334726ce20b1b825bd6d26cc.

--- crf_pos/api.py
+++ crf_pos/api.py
@@ -1,10 +1,10 @@
 import requests
 
 
-def downloader(path: str, save_path: str = 'model/perpos-v1.model', mode: str = 'wb'):
+def downloader(path: str, save_path: str, mode: str):
     try:
         model_bin = requests.get(path, allow_redirects=True)
         with open(save_path, mode) as resource:
             resource.write(model_bin.content)
-    except Exception as e:
+    except Exception:
         raise Exception('not a proper webpage')
