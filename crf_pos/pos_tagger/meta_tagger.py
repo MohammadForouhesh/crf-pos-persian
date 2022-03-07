be1b6197e3b79b2b96fcd163305c6832fb302dc7
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-07 10:03:06 +0330

    tests passed

--- crf_pos/pos_tagger/meta_tagger.py
+++ crf_pos/pos_tagger/meta_tagger.py
@@ -2,15 +2,14 @@ from typing import Union, List, Any, Tuple
 from crf_pos.normalization.normalizer import Normalizer
 from crf_pos.normalization.tokenizer import tokenize_words
 
-norm = Normalizer()
-
 
 class MetaTagger:
     def __init__(self) -> None:
         self.tagger = None
+        self.norm = Normalizer()
 
     def __getitem__(self, item: Union[list, str]) -> List[Tuple[str, str]]:
-        if isinstance(item, str):   item = tokenize_words(norm.normalize(item))
+        if isinstance(item, str):   item = tokenize_words(self.norm.normalize(item))
         return self.parse([item])[0]
 
     def parse(self, token_list: List[str]) -> List[List[Tuple[Any, Any]]]:
