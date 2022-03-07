ace9438aeaa0f7da567c9f1d4a0aca2bc753a4ed
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-07 10:51:45 +0330

    Versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='crf_pos',
-    version='1.0.0',
+    version='2.0.0a',
     packages=['crf_pos'],
     url='https://github.com/MohammadForouhesh/crf-pos-persian',
     license='MIT',
