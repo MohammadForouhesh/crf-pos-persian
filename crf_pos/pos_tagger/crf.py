6a66a1092b994d5fa1b3b87e97a196c9bb4eee1b
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-08 13:38:57 +0330

    module documentation for crf.py

--- crf_pos/pos_tagger/crf.py
+++ crf_pos/pos_tagger/crf.py
@@ -1,3 +1,15 @@
+"""
+CRF Classifier
+
+..................................................................................................................
+MIT License
+
+Copyright (c) 2021-2023 AUT Iran, Mohammad H Forouhesh
+Copyright (c) 2021-2022 MetoData.ai, Mohammad H Forouhesh
+..................................................................................................................
+This Module contains the implementation and encapsulation for Conditional Random Field classifier.
+"""
+
 from typing import List, Tuple, Any
 from crf_pos.pos_tagger.meta_tagger import MetaTagger
 from crf_pos.pos_tagger.utils import token2features
