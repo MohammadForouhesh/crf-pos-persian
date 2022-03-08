79ac49b296ecddc951e305c499d63941929984f1
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-08 15:25:03 +0330

    adding class/method docstring for wapiti.py

--- crf_pos/pos_tagger/wapiti.py
+++ crf_pos/pos_tagger/wapiti.py
@@ -17,11 +17,20 @@ from crf_pos.pos_tagger.utils import remove_after_underline
 
 
 class WapitiPosTagger(MetaTagger):
+    """
+    Wapiti Part-of-Speech tagger encapsulation.
+    """
     def __init__(self, model_path: str = 'model/UPC_full_model_wapiti') -> None:
         super().__init__()
         self.tagger = Model(model=model_path)
 
     def parse(self, token_list: List[str]) -> List[List[Tuple[Any, Any]]]:
+        """
+        Primary function that override the same method from the super class. This function is responsible for the logic
+        behind the model.
+        :param token_list:  A list of tokens (strings)
+        :return:            A list of extracted part of speeches and their related tokens.
+        """
         sent_line = "\n".join(token_list[0])
         postags = self.tagger.label_sequence(sent_line).decode('utf-8').strip().split('\n')
         return list(super().zip_vector(zip(token_list, [list(map(remove_after_underline, postags))])))
