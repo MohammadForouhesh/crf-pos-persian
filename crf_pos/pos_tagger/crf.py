0bfef941da294d50ab39f4b9c2c7929155e0d8fe
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-08 15:24:09 +0330

    adding class/method docstring for crf.py

--- crf_pos/pos_tagger/crf.py
+++ crf_pos/pos_tagger/crf.py
@@ -17,11 +17,20 @@ import pickle
 
 
 class CrfPosTagger(MetaTagger):
+    """
+    Wapiti Part-of-Speech tagger encapsulation.
+    """
     def __init__(self, model_path) -> None:
         super().__init__()
         with open(model_path, 'rb') as resource:
             self.tagger = pickle.load(resource)
 
     def parse(self, token_list: List[str]) -> List[List[Tuple[Any, Any]]]:
+        """
+        Primary function that override the same method from the super class. This function is responsible for the logic
+        behind the model.
+        :param token_list:  A list of tokens (strings)
+        :return:            A list of extracted part of speeches and their related tokens.
+        """
         y_pred = self.tagger.predict([token2features(token) for token in token_list])
         return list(super().zip_vector(zip(token_list, y_pred)))
